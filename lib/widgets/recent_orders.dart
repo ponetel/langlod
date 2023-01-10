ac3e3a9f4f3140c3098b3ef2dde0789d17f464fd
ponetel <ponetel@gmail.com>
2023-01-11 06:30:33 +0700

    Update recent_orders.dart

--- lib/widgets/recent_orders.dart
+++ lib/widgets/recent_orders.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
+import 'package:langlod/models/order.dart';
 import '../data/data.dart';
 
 class RecentOrders extends StatelessWidget {
   const RecentOrders({super.key});
+  _buildRecentOrder(BuildContext context, Order order) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      width: 320.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        border: Border.all(
+          width: 1.0,
+          color: Colors.grey,
+        ),
+      ),
+      child: Row(
+        children: [Image(image: AssetImage(order.shops.imageUrl))],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +40,18 @@ class RecentOrders extends StatelessWidget {
             ),
           ),
         ),
-        // Container(
-        //   height: 120.0,
-        //   color: Colors.blue,
-        //   child: ListView.builder(itemCount: currentUser.orders.length,
-        //     itemBuilder: (BuildContext context, int index) {
-        //       Order order = currentUser.orders[index];
-        //       return Text(orders.);},),),
-        // )
+        Container(
+          height: 120.0,
+          //color: Colors.blue,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: currentUser.orders.length,
+            itemBuilder: (BuildContext context, int index) {
+              Order order = currentUser.orders[index];
+              return _buildRecentOrder(context, order);
+            },
+          ),
+        ),
       ],
     );
   }
